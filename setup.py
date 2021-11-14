f96b2bc6147b4af597862077a934a389dbaf53f0
tbainesUA <tylerbaines@email.arizona.edu>
2021-11-13 21:09:00 -0800

    updated information

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='crispy',
-    version="0.9", 
-    author='Maxime Rizzo',
-    author_email = 'maxime.j.rizzo@nasa.gov',
-    url = 'https://github.com/mjrfringes/crispy',
+    version="1.0.0", 
+    author='Tyler Baines',
+    author_email = 'tyler.baines@nasa.gov',
+    url = 'https://github.com/tbainesUA/crispy',
     packages =['crispy'],
     license = ['GNU GPLv3'],
     description ='The Coronagraph and Rapid Imaging Spectrograph in Python',
